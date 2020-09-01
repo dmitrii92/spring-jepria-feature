7ed027c6f2c0117f6411d232c97d9cf0a8df9a33
dmitrii92 <fogot92@gmail.com>
2020-09-01 18:45:38 +0300

    test(FeatureControllerTest): add new tests

--- src/test/java/com/jepria/springstarter/feature/controller/FeatureControllerTest.java
+++ src/test/java/com/jepria/springstarter/feature/controller/FeatureControllerTest.java
@@ -1,18 +1,26 @@
 package com.jepria.springstarter.feature.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.jepria.springstarter.feature.dto.FeatureCreateDto;
 import com.jepria.springstarter.feature.dto.FeatureDto;
+import com.jepria.springstarter.feature.dto.FeatureStatusDto;
+import com.jepria.springstarter.feature.dto.FeatureUpdateDto;
 import com.jepria.springstarter.feature.service.FeatureService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = FeatureController.class)
@@ -52,5 +60,61 @@ public class FeatureControllerTest {
             .contentType("application/json")
             .content(objectMapper.writeValueAsString(dto)))
         .andExpect(status().isCreated());
+  }
+
+  @Test
+  public void updateFeatureTest() throws Exception {
+    int featureId = 10;
+    FeatureUpdateDto dto = new FeatureUpdateDto();
+    dto.setFeatureName("featureName");
+    dto.setDescription("description");
+    when(service.update(10, dto)).thenReturn(true);
+
+    mockMvc.perform(put("/feature/" + featureId)
+        .contentType("application/json")
+        .content(objectMapper.writeValueAsString(dto)))
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  public void deleteFeatureTest() throws Exception {
+    mockMvc.perform(delete("/feature/10")).andExpect(status().isOk());
+  }
+
+  @Test
+  public void getAllFeaturesTest() throws Exception {
+    List<FeatureDto> dtos = new ArrayList<>();
+    dtos.add(new FeatureDto());
+    when(service.getAll()).thenReturn((dtos));
+
+    mockMvc.perform(get("/feature")).andExpect(status().isOk());
+  }
+
+  @Test
+  public void getFeatureStatusOptionsTest() throws Exception {
+    List<FeatureStatusDto> featureStatusOptions = new ArrayList<>();
+    featureStatusOptions.add(new FeatureStatusDto("new", "New"));
+    featureStatusOptions.add(new FeatureStatusDto("assign", "Assign"));
+
+    when(service.getStatusOptions()).thenReturn(featureStatusOptions);
+    MvcResult result = mockMvc.perform(get("/feature/option/feature-status"))
+        .andExpect(status().isOk()).andReturn();
+
+    Gson gson = new Gson();
+
+    Assertions.assertEquals(gson.toJson(featureStatusOptions), result.getResponse().getContentAsString());
+  }
+
+  @Test
+  public void setFeatureStatusOptionsTest() throws Exception {
+    List<FeatureStatusDto> featureStatusOptions = new ArrayList<>();
+    featureStatusOptions.add(new FeatureStatusDto("new", "New"));
+    featureStatusOptions.add(new FeatureStatusDto("assign", "Assign"));
+
+    MvcResult result = mockMvc.perform(
+        post("/feature/option/feature-status")
+            .contentType("application/json")
+            .content(objectMapper.writeValueAsString(featureStatusOptions)))
+        .andExpect(status().isOk()).andReturn();
   }
 }
